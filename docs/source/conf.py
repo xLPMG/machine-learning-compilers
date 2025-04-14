390190881a5608659df8bc1d8084169965736f32
AsZen <92991272+Shad00Z@users.noreply.github.com>
2025-04-14 16:19:26 +0200

    Adding a setup for sphinx and doxygen for code documentation

---
+++ docs/source/conf.py
@@ -0,0 +1,65 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+from datetime import date
+import subprocess
+import os
+
+# Doxygen
+if not os.path.exists( "_build/xml/" ):
+    subprocess.call( 'doxygen ../Doxyfile.in', 
+                     shell=True )
+
+project = 'Machine Learning Compilers'
+copyright = f'{date.today().year}, Lucas Obitz, Luca-Philipp Krumbach'
+author = 'Lucas Obitz, Luca-Philipp Krumbach'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'breathe'
+]
+
+templates_path = [
+    '_templates'
+]
+exclude_patterns = [
+    '_build',
+    '.DS_Store'
+]
+
+highlight_language = 'c++'
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'furo'
+html_theme_options = {
+    "source_repository": "https://github.com/Shad00Z/machine-learning-compilers/",
+    "source_branch": "main",
+    "source_directory": "docs/",
+}
+html_static_path = [
+    '_static'
+]
+
+# -- Breathe configuration -------------------------------------------------
+
+breathe_projects = {
+    "Machine Learning Compilers": "_build/xml/",
+}
+
+breathe_default_project = "Machine Learning Compilers"
+breathe_default_members = (
+    'members', 
+    'undoc-members'
+)
