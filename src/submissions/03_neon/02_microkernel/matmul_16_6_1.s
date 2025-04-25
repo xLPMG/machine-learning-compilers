7e60cb3baa560fbb4b59869fdd3208324a55e393
Luca-Philipp Grumbach <xxlpmghdxx@gmail.com>
2025-04-25 14:27:07 +0200

    implement microkernel

---
+++ src/submissions/03_neon/02_microkernel/matmul_16_6_1.s
@@ -0,0 +1,174 @@
+    .text
+    .global _matmul_16_6_1
+    /*
+    * Computes C+=AB for three matrices 
+    * with the dimensions M=16, N=6, and K=1.
+    *
+    * @param x0 pointer to column-major matrix A.
+    * @param x1 pointer to column-major matrix B.
+    * @param x2 pointer to column-major matrix C.
+    * @param x3 leading dimension of A.
+    * @param x4 leading dimension of B.
+    * @param x5 leading dimension of C.
+    */
+_matmul_16_6_1:
+    // save frame pointer and link register
+    stp fp, lr, [sp, #-16]!
+    // update frame pointer to current stack pointer
+    mov fp, sp
+
+    // save callee-saved registers
+    stp x19, x20, [sp, #-16]!
+    stp x21, x22, [sp, #-16]!
+    stp x23, x24, [sp, #-16]!
+    stp x25, x26, [sp, #-16]!
+    stp x27, x28, [sp, #-16]!
+
+    stp  d8,  d9, [sp, #-16]!
+    stp d10, d11, [sp, #-16]!
+    stp d12, d13, [sp, #-16]!
+    stp d14, d15, [sp, #-16]!
+
+    // multiply strides with float size
+    mov x6, #4
+    mul x3, x3, x6
+    mul x4, x4, x6
+    mul x5, x5, x6
+
+    // load full matrix A
+    ldp q0, q1, [x0] // 4 + 4 values
+    ldp q2, q3, [x0, #32] // 4 + 4 values
+
+    // current column of B
+    mov x6, x1
+    // current column of C
+    mov x7, x2
+
+
+    // C: COLUMN 0
+    // -------------------------
+    // Load column of B
+    ldr s8, [x6]
+    // Load column of C
+    ldp q4, q5, [x7]
+    ldp q6, q7, [x7, #32]
+    // Multiply and accumulate
+    fmla v4.4s, v0.4s, v8.s[0]
+    fmla v5.4s, v1.4s, v8.s[0]
+    fmla v6.4s, v2.4s, v8.s[0]
+    fmla v7.4s, v3.4s, v8.s[0]
+    // Store column of C
+    stp q4, q5, [x7]
+    stp q6, q7, [x7, #32]
+
+    // C: COLUMN 1
+    // -------------------------
+    // Increment current columns
+    add x6, x6, x4
+    add x7, x7, x5
+    // Load column of B
+    ldr s8, [x6]
+    // Load column of C
+    ldp q4, q5, [x7]
+    ldp q6, q7, [x7, #32]
+    // Multiply and accumulate
+    fmla v4.4s, v0.4s, v8.s[0]
+    fmla v5.4s, v1.4s, v8.s[0]
+    fmla v6.4s, v2.4s, v8.s[0]
+    fmla v7.4s, v3.4s, v8.s[0]
+    // Store column of C
+    stp q4, q5, [x7]
+    stp q6, q7, [x7, #32]
+
+    // C: COLUMN 2
+    // -------------------------
+    // Increment current columns
+    add x6, x6, x4
+    add x7, x7, x5
+    // Load column of B
+    ldr s8, [x6]
+    // Load column of C
+    ldp q4, q5, [x7]
+    ldp q6, q7, [x7, #32]
+    // Multiply and accumulate
+    fmla v4.4s, v0.4s, v8.s[0]
+    fmla v5.4s, v1.4s, v8.s[0]
+    fmla v6.4s, v2.4s, v8.s[0]
+    fmla v7.4s, v3.4s, v8.s[0]
+    // Store column of C
+    stp q4, q5, [x7]
+    stp q6, q7, [x7, #32]
+
+    // C: COLUMN 3
+    // -------------------------
+    // Increment current columns
+    add x6, x6, x4
+    add x7, x7, x5
+    // Load column of B
+    ldr s8, [x6]
+    // Load column of C
+    ldp q4, q5, [x7]
+    ldp q6, q7, [x7, #32]
+    // Multiply and accumulate
+    fmla v4.4s, v0.4s, v8.s[0]
+    fmla v5.4s, v1.4s, v8.s[0]
+    fmla v6.4s, v2.4s, v8.s[0]
+    fmla v7.4s, v3.4s, v8.s[0]
+    // Store column of C
+    stp q4, q5, [x7]
+    stp q6, q7, [x7, #32]
+
+    // C: COLUMN 4
+    // -------------------------
+    // Increment current columns
+    add x6, x6, x4
+    add x7, x7, x5
+    // Load column of B
+    ldr s8, [x6]
+    // Load column of C
+    ldp q4, q5, [x7]
+    ldp q6, q7, [x7, #32]
+    // Multiply and accumulate
+    fmla v4.4s, v0.4s, v8.s[0]
+    fmla v5.4s, v1.4s, v8.s[0]
+    fmla v6.4s, v2.4s, v8.s[0]
+    fmla v7.4s, v3.4s, v8.s[0]
+    // Store column of C
+    stp q4, q5, [x7]
+    stp q6, q7, [x7, #32]
+
+    // C: COLUMN 5
+    // -------------------------
+    // Increment current columns
+    add x6, x6, x4
+    add x7, x7, x5
+    // Load column of B
+    ldr s8, [x6]
+    // Load column of C
+    ldp q4, q5, [x7]
+    ldp q6, q7, [x7, #32]
+    // Multiply and accumulate
+    fmla v4.4s, v0.4s, v8.s[0]
+    fmla v5.4s, v1.4s, v8.s[0]
+    fmla v6.4s, v2.4s, v8.s[0]
+    fmla v7.4s, v3.4s, v8.s[0]
+    // Store column of C
+    stp q4, q5, [x7]
+    stp q6, q7, [x7, #32]
+
+    // restore callee-saved registers
+    ldp d14, d15, [sp], #16
+    ldp d12, d13, [sp], #16
+    ldp d10, d11, [sp], #16
+    ldp  d8,  d9, [sp], #16
+
+    ldp x27, x28, [sp], #16
+    ldp x25, x26, [sp], #16
+    ldp x23, x24, [sp], #16
+    ldp x21, x22, [sp], #16
+    ldp x19, x20, [sp], #16
+
+    // restore frame pointer and link register
+    ldp fp, lr, [sp], #16
+
+    ret
---
+++ src/submissions/03_neon/02_microkernel/microkernel.cpp
@@ -0,0 +1,85 @@
+#include <cstdint>
+#include <iostream>
+
+extern "C"
+{
+    /**
+     * @param a pointer to column-major matrix A.
+     * @param b pointer to column-major matrix B.
+     * @param c pointer to column-major matrix C.
+     * @param lda leading dimension of A.
+     * @param ldb leading dimension of B.
+     * @param ldc leading dimension of C.
+     **/
+    void matmul_16_6_1(float const *a,
+                       float const *b,
+                       float *c,
+                       int64_t lda,
+                       int64_t ldb,
+                       int64_t ldc);
+}
+
+int main()
+{
+
+    const int M = 16;
+    const int N = 6;
+    const int K = 1;
+
+    float A[M * K];
+    float B[K * N];
+    float C[M * N];
+
+    // Initialize matrices
+    for (int i = 0; i < M * K; ++i)
+    {
+        A[i] = static_cast<float>(i);
+    }
+    for (int j = 0; j < K * N; ++j)
+    {
+        B[j] = static_cast<float>(j);
+    }
+    std::memset(C, 0, sizeof(C));
+
+    // Print matrix A
+    std::cout << "Matrix A:" << std::endl;
+    for (int i = 0; i < M; ++i)
+    {
+        for (int j = 0; j < K; ++j)
+        {
+            std::cout << A[i * K + j] << " ";
+        }
+        std::cout << "\n";
+    }
+
+    // Print matrix B
+    std::cout << "Matrix B:" << std::endl;
+    for (int i = 0; i < K; ++i)
+    {
+        for (int j = 0; j < N; ++j)
+        {
+            std::cout << B[i * N + j] << " ";
+        }
+        std::cout << "\n";
+    }
+
+    matmul_16_6_1(A,
+                  B,
+                  C,
+                  16,
+                  1,
+                  16);
+
+    // Print the result
+    std::cout << "Result Matrix C:" << std::endl;
+    for (int row = 0; row < M; row++)
+    {
+        for (int col = 0; col < N; col++)
+        {
+            std::cout << C[col * M + row] << " ";
+        }
+        std::cout << std::endl;
+    }
+
+    return 0;
+}
