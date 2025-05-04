a01a39eadde9cc3e9c690319ba6390579741035e
Lucas Obitz <92991272+Shad00Z@users.noreply.github.com>
2025-05-04 10:46:52 +0200

    feat: optimization, benchmarking and documentation (#10)

---
+++ src/submissions/03_neon/04_simd/matmul_15_6_64/v3_matmul_15_6_64.s
@@ -0,0 +1,180 @@
+    .text
+    .type v3_matmul_15_6_64, %function
+    .global v3_matmul_15_6_64
+    /*
+    * Computes C+=AB for three matrices 
+    * with the dimensions M=15, N=6, and K=64.
+    *
+    * Idea:
+    * Equal to Version 2 but with different loads
+    *
+    * @param x0 pointer to column-major matrix A.
+    * @param x1 pointer to column-major matrix B.
+    * @param x2 pointer to column-major matrix C.
+    * @param x3 leading dimension of A.
+    * @param x4 leading dimension of B.
+    * @param x5 leading dimension of C.
+    */
+v3_matmul_15_6_64:
+// #################### START PCS ####################
+    // save frame pointer and link register
+    stp fp, lr, [sp, #-16]!
+    // update frame pointer to current stack pointer
+    mov fp, sp
+
+    // save callee-saved registers
+    stp x19, x20, [sp, #-16]!
+    stp x21, x22, [sp, #-16]!
+    stp x23, x24, [sp, #-16]!
+    stp x25, x26, [sp, #-16]!
+    stp x27, x28, [sp, #-16]!
+
+    stp  d8,  d9, [sp, #-16]!
+    stp d10, d11, [sp, #-16]!
+    stp d12, d13, [sp, #-16]!
+    stp d14, d15, [sp, #-16]!
+// #################### END PCS ####################
+
+    // multiply strides with float size
+    mov x6, #4
+    mul x3, x3, x6 // lda
+    mul x4, x4, x6 // ldb
+    mul x5, x5, x6 // ldc
+
+    // LOAD MATRIX C
+    mov x8, x2
+    // first column
+    ld1 {v0.4s-v3.4s},   [x8]
+    mov v3.s[3], wzr
+    add x8, x8, x5
+    // second column
+    ld1 {v4.4s-v7.4s},   [x8]
+    mov v7.s[3], wzr
+    add x8, x8, x5
+    // third column
+    ld1 {v8.4s-v11.4s},  [x8]
+    mov v11.s[3], wzr
+    add x8, x8, x5
+    // fourth column
+    ld1 {v12.4s-v15.4s}, [x8]
+    mov v15.s[3], wzr
+    add x8, x8, x5
+    // fifth column
+    ld1 {v16.4s-v19.4s}, [x8]
+    mov v19.s[3], wzr
+    add x8, x8, x5
+    // sixth column
+    ld1 {v20.4s-v23.4s}, [x8]
+    mov v23.s[3], wzr
+
+
+    //  K loop counter
+    mov x6, #64
+    // set start of A
+    mov x7, x0
+    // set start of B
+    mov x8, x1
+    // init row count of B
+    mov x9, #0
+    
+
+_k1_loop:
+    // load column of A
+    ldp q24, q25, [x7] // 4 + 4 values
+    ldp q26, q27, [x7, #32] // 4 + 4 values
+    mov v27.s[3], wzr
+
+    // B: COLUMN 0
+    ldr s29, [x8]
+    
+    fmla v0.4s, v24.4s, v29.s[0]
+    fmla v1.4s, v25.4s, v29.s[0]
+    fmla v2.4s, v26.4s, v29.s[0]
+    fmla v3.4s, v27.4s, v29.s[0]
+    // B: COLUMN 1
+    add x8, x8, x4
+    ldr s29, [x8]
+    fmla v4.4s, v24.4s, v29.s[0]
+    fmla v5.4s, v25.4s, v29.s[0]
+    fmla v6.4s, v26.4s, v29.s[0]
+    fmla v7.4s, v27.4s, v29.s[0]
+    // B: COLUMN 2
+    add x8, x8, x4
+    ldr s29, [x8]
+    fmla  v8.4s, v24.4s, v29.s[0]
+    fmla  v9.4s, v25.4s, v29.s[0]
+    fmla v10.4s, v26.4s, v29.s[0]
+    fmla v11.4s, v27.4s, v29.s[0]
+    // B: COLUMN 3
+    add x8, x8, x4
+    ldr s29, [x8]
+    fmla v12.4s, v24.4s, v29.s[0]
+    fmla v13.4s, v25.4s, v29.s[0]
+    fmla v14.4s, v26.4s, v29.s[0]
+    fmla v15.4s, v27.4s, v29.s[0]
+    // B: COLUMN 4
+    add x8, x8, x4
+    ldr s29, [x8]
+    fmla v16.4s, v24.4s, v29.s[0]
+    fmla v17.4s, v25.4s, v29.s[0]
+    fmla v18.4s, v26.4s, v29.s[0]
+    fmla v19.4s, v27.4s, v29.s[0]
+    // B: COLUMN 5
+    add x8, x8, x4
+    ldr s29, [x8]
+    fmla v20.4s, v24.4s, v29.s[0]
+    fmla v21.4s, v25.4s, v29.s[0]
+    fmla v22.4s, v26.4s, v29.s[0]
+    fmla v23.4s, v27.4s, v29.s[0]
+
+    // move to next column of A
+    add x7, x7, x3
+    // move to next row of B
+    mov x8, x1
+    add x9, x9, #4
+    add x8, x8, x9
+
+    // decrement loop counter
+    sub x6, x6, #1
+    // check if loop counter is zero
+    cbnz x6, _k1_loop
+
+
+    // STORE MATRIX C
+    mov x8, x2
+    // first column
+    st1 {v0.4s-v3.4s},   [x8]
+    add x8, x8, x5
+    // second column
+    st1 {v4.4s-v7.4s},   [x8]
+    add x8, x8, x5
+    // third column
+    st1 {v8.4s-v11.4s},  [x8]
+    add x8, x8, x5
+    // fourth column
+    st1 {v12.4s-v15.4s}, [x8]
+    add x8, x8, x5
+    // fifth column
+    st1 {v16.4s-v19.4s}, [x8]
+    add x8, x8, x5
+    // sixth column
+    st1 {v20.4s-v23.4s}, [x8]
+
+// #################### START PCS ####################
+    // restore callee-saved registers
+    ldp d14, d15, [sp], #16
+    ldp d12, d13, [sp], #16
+    ldp d10, d11, [sp], #16
+    ldp  d8,  d9, [sp], #16
+
+    ldp x27, x28, [sp], #16
+    ldp x25, x26, [sp], #16
+    ldp x23, x24, [sp], #16
+    ldp x21, x22, [sp], #16
+    ldp x19, x20, [sp], #16
+
+    // restore frame pointer and link register
+    ldp fp, lr, [sp], #16
+// #################### END PCS ####################
+    ret
+    
---
+++ src/submissions/03_neon/04_simd/microbench.cpp
@@ -0,0 +1,487 @@
+#include <arm_neon.h>
+#include <iostream>
+#include <cstdint>
+#include <chrono>
+#include <cstring>
+
+extern "C" {
+    void v1_matmul_14_6_64( float const *a,
+                            float const *b,
+                            float *c,
+                            int64_t lda,
+                            int64_t ldb,
+                            int64_t ldc );
+
+    void v2_matmul_14_6_64( float const *a,
+                            float const *b,
+                            float *c,
+                            int64_t lda,
+                            int64_t ldb,
+                            int64_t ldc );
+
+    void v3_matmul_14_6_64( float const *a,
+                            float const *b,
+                            float *c,
+                            int64_t lda,
+                            int64_t ldb,
+                            int64_t ldc );
+
+    void v4_matmul_14_6_64( float const *a,
+                            float const *b,
+                            float *c,
+                            int64_t lda,
+                            int64_t ldb,
+                            int64_t ldc );
+
+    void v1_matmul_15_6_64( float const *a,
+                            float const *b,
+                            float *c,
+                            int64_t lda,
+                            int64_t ldb,
+                            int64_t ldc );
+
+    void v2_matmul_15_6_64( float const *a,
+                            float const *b,
+                            float *c,
+                            int64_t lda,
+                            int64_t ldb,
+                            int64_t ldc );
+
+    void v3_matmul_15_6_64( float const *a,
+                            float const *b,
+                            float *c,
+                            int64_t lda,
+                            int64_t ldb,
+                            int64_t ldc );
+
+    void matmul_M_6_64( float const *a,
+                        float const *b,
+                        float *c,
+                        int64_t lda,
+                        int64_t ldb,
+                        int64_t ldc );
+}
+
+/*
+ * Benchmarks the throughput for the different MatMul (loop) versions.
+ *
+ * @param n: number of loop iterations.
+ * @param instruction: a string selecting the instruction to benchmark.
+ * @param a pointer to column-major matrix A.
+ * @param b pointer to column-major matrix B.
+ * @param c pointer to column-major matrix C.
+ */
+ void benchmark_thr( int64_t n, 
+                     std::string instruction,
+                     float const *a,
+                     float const *b,
+                     float *c ) 
+{
+    std::cout << "-----------------------------------------------\n";
+    double elapsedTime = 1;
+
+    std::string v1_matmul_14( "v1_matmul_14" );
+    int res_1 = v1_matmul_14.compare( instruction );
+
+    std::string v2_matmul_14( "v2_matmul_14" );
+    int res_2 = v2_matmul_14.compare( instruction );
+
+    std::string v3_matmul_14( "v3_matmul_14" );
+    int res_3 = v3_matmul_14.compare( instruction );
+
+    std::string v4_matmul_14( "v4_matmul_14" );
+    int res_4 = v4_matmul_14.compare( instruction );
+
+    std::string v1_matmul_15( "v1_matmul_15" );
+    int res_5 = v1_matmul_15.compare( instruction );
+
+    std::string v2_matmul_15( "v2_matmul_15" );
+    int res_6 = v2_matmul_15.compare( instruction );
+
+    std::string v3_matmul_15( "v3_matmul_15" );
+    int res_7 = v3_matmul_15.compare( instruction );
+
+    std::string g_matmul_14( "g_matmul_14" );
+    int res_8 = g_matmul_14.compare( instruction );
+
+    std::string g_matmul_15( "g_matmul_15" );
+    int res_9 = g_matmul_15.compare( instruction );
+
+    double opsPerMatmul = 1;
+
+    // Time measuring
+    if ( res_1 == 0 )
+    {
+        // Warmup
+        for ( int i = 0; i < 1000; i++ )
+        {
+            v1_matmul_14_6_64( a, 
+                            b, 
+                            c, 
+                            14, 
+                            64, 
+                            14 );
+        }
+        std::memset( c, 0, sizeof( c ) );
+
+        auto l_start_time = std::chrono::high_resolution_clock::now();
+        for ( int i = 0; i < n; i++ )
+        {
+            v1_matmul_14_6_64( a, 
+                            b, 
+                            c, 
+                            14, 
+                            64, 
+                            14 );
+        }
+        auto l_end_time = std::chrono::high_resolution_clock::now();
+        elapsedTime = std::chrono::duration_cast<std::chrono::microseconds>( l_end_time - l_start_time ).count() / 1e6;
+
+        // (M x N x K) x (FMLA OPs)
+        opsPerMatmul = ( 14 * 6 * 64 ) * 2;
+    }
+    else if ( res_2 == 0 )
+    {
+        // Warmup
+        for ( int i = 0; i < 1000; i++ )
+        {
+            v2_matmul_14_6_64( a, 
+                            b, 
+                            c, 
+                            14, 
+                            64, 
+                            14 );
+        }
+        std::memset( c, 0, sizeof( c ) );
+
+        auto l_start_time = std::chrono::high_resolution_clock::now();
+        for ( int i = 0; i < n; i++ )
+        {
+            v2_matmul_14_6_64( a, 
+                            b, 
+                            c, 
+                            14, 
+                            64, 
+                            14 );
+        }
+        auto l_end_time = std::chrono::high_resolution_clock::now();
+        elapsedTime = std::chrono::duration_cast<std::chrono::microseconds>( l_end_time - l_start_time ).count() / 1e6;
+
+        // (M x N x K) x (FMLA OPs)
+        opsPerMatmul = ( 14 * 6 * 64 ) * 2;
+    }
+    else if ( res_3 == 0 )
+    {
+        // Warmup
+        for ( int i = 0; i < 1000; i++ )
+        {
+            v3_matmul_14_6_64( a, 
+                             b, 
+                             c, 
+                             14, 
+                             64, 
+                             14 );
+        }
+        std::memset( c, 0, sizeof( c ) );
+
+        auto l_start_time = std::chrono::high_resolution_clock::now();
+        for ( int i = 0; i < n; i++ )
+        {
+            v3_matmul_14_6_64( a, 
+                             b, 
+                             c, 
+                             14, 
+                             64, 
+                             14 );
+        }
+        auto l_end_time = std::chrono::high_resolution_clock::now();
+        elapsedTime = std::chrono::duration_cast<std::chrono::microseconds>( l_end_time - l_start_time ).count() / 1e6;
+
+        // (M x N x K) x (FMLA OPs)
+        opsPerMatmul = ( 14 * 6 * 64 ) * 2;
+    }
+    else if ( res_4 == 0 )
+    {
+        // Warmup
+        for ( int i = 0; i < 1000; i++ )
+        {
+            v4_matmul_14_6_64( a, 
+                             b, 
+                             c, 
+                             14, 
+                             64, 
+                             14 );
+        }
+        std::memset( c, 0, sizeof( c ) );
+
+        auto l_start_time = std::chrono::high_resolution_clock::now();
+        for ( int i = 0; i < n; i++ )
+        {
+            v4_matmul_14_6_64( a, 
+                             b, 
+                             c, 
+                             14, 
+                             64, 
+                             14 );
+        }
+        auto l_end_time = std::chrono::high_resolution_clock::now();
+        elapsedTime = std::chrono::duration_cast<std::chrono::microseconds>( l_end_time - l_start_time ).count() / 1e6;
+
+        // (M x N x K) x (FMLA OPs)
+        opsPerMatmul = ( 14 * 6 * 64 ) * 2;
+    }
+    else if ( res_5 == 0 )
+    {
+        // Warmup
+        for ( int i = 0; i < 1000; i++ )
+        {
+            v1_matmul_15_6_64( a, 
+                             b, 
+                             c, 
+                             15, 
+                             64, 
+                             15 );
+        }
+        std::memset( c, 0, sizeof( c ) );
+
+        auto l_start_time = std::chrono::high_resolution_clock::now();
+        for ( int i = 0; i < n; i++ )
+        {
+            v1_matmul_15_6_64( a, 
+                             b, 
+                             c, 
+                             15, 
+                             64, 
+                             15 );
+        }
+        auto l_end_time = std::chrono::high_resolution_clock::now();
+        elapsedTime = std::chrono::duration_cast<std::chrono::microseconds>( l_end_time - l_start_time ).count() / 1e6;
+
+        // (M x N x K) x (FMLA OPs)
+        opsPerMatmul = ( 15 * 6 * 64 ) * 2;
+    }
+    else if ( res_6 == 0 )
+    {
+        // Warmup
+        for ( int i = 0; i < 1000; i++ )
+        {
+            v2_matmul_15_6_64( a, 
+                             b, 
+                             c, 
+                             15, 
+                             64, 
+                             15 );
+        }
+        std::memset( c, 0, sizeof( c ) );
+
+        auto l_start_time = std::chrono::high_resolution_clock::now();
+        for ( int i = 0; i < n; i++ )
+        {
+            v2_matmul_15_6_64( a, 
+                             b, 
+                             c, 
+                             15, 
+                             64, 
+                             15 );
+        }
+        auto l_end_time = std::chrono::high_resolution_clock::now();
+        elapsedTime = std::chrono::duration_cast<std::chrono::microseconds>( l_end_time - l_start_time ).count() / 1e6;
+
+        // (M x N x K) x (FMLA OPs)
+        opsPerMatmul = ( 15 * 6 * 64 ) * 2;
+    }
+    else if ( res_7 == 0 )
+    {
+        // Warmup
+        for ( int i = 0; i < 1000; i++ )
+        {
+            v3_matmul_15_6_64( a, 
+                             b, 
+                             c, 
+                             15, 
+                             64, 
+                             15 );
+        }
+        std::memset( c, 0, sizeof( c ) );
+
+        auto l_start_time = std::chrono::high_resolution_clock::now();
+        for ( int i = 0; i < n; i++ )
+        {
+            v3_matmul_15_6_64( a, 
+                             b, 
+                             c, 
+                             15, 
+                             64, 
+                             15 );
+        }
+        auto l_end_time = std::chrono::high_resolution_clock::now();
+        elapsedTime = std::chrono::duration_cast<std::chrono::microseconds>( l_end_time - l_start_time ).count() / 1e6;
+
+        // (M x N x K) x (FMLA OPs)
+        opsPerMatmul = ( 15 * 6 * 64 ) * 2;
+    }
+    else if ( res_8 == 0 )
+    {
+        // Warmup
+        for ( int i = 0; i < 1000; i++ )
+        {
+            matmul_M_6_64( a, 
+                           b, 
+                           c, 
+                           14, 
+                           64, 
+                           14 );
+        }
+        std::memset( c, 0, sizeof( c ) );
+
+        auto l_start_time = std::chrono::high_resolution_clock::now();
+        for ( int i = 0; i < n; i++ )
+        {
+            matmul_M_6_64( a, 
+                           b, 
+                           c, 
+                           14, 
+                           64, 
+                           14 );
+        }
+        auto l_end_time = std::chrono::high_resolution_clock::now();
+        elapsedTime = std::chrono::duration_cast<std::chrono::microseconds>( l_end_time - l_start_time ).count() / 1e6;
+
+        // (M x N x K) x (FMLA OPs)
+        opsPerMatmul = ( 14 * 6 * 64 ) * 2;
+    }
+    else if ( res_9 == 0 )
+    {
+        // Warmup
+        for ( int i = 0; i < 1000; i++ )
+        {
+            matmul_M_6_64( a, 
+                           b, 
+                           c, 
+                           15, 
+                           64, 
+                           15 );
+        }
+        std::memset( c, 0, sizeof( c ) );
+
+        auto l_start_time = std::chrono::high_resolution_clock::now();
+        for ( int i = 0; i < n; i++ )
+        {
+            matmul_M_6_64( a, 
+                           b, 
+                           c, 
+                           15, 
+                           64, 
+                           15 );
+        }
+        auto l_end_time = std::chrono::high_resolution_clock::now();
+        elapsedTime = std::chrono::duration_cast<std::chrono::microseconds>( l_end_time - l_start_time ).count() / 1e6;
+
+        // (M x N x K) x (FMLA OPs)
+        opsPerMatmul = ( 15 * 6 * 64 ) * 2;
+    }
+
+    double loopIterations = n;
+    double totalFLOPs = opsPerMatmul * loopIterations;
+
+    double flopsPerSec = totalFLOPs / elapsedTime;
+    double gflops = totalFLOPs / ( elapsedTime * 1e9 );
+
+    std::cout << "Measuring throughput for " << "Instruction\n";
+    std::cout << "Total time (s):   " << elapsedTime << "\n";
+    std::cout << "Instructions per Second:   " << flopsPerSec << "\n";
+    std::cout << "Estimated GFLOPS:   " << gflops << " GFLOPS/sec\n";
+    std::cout << "-----------------------------------------------\n";
+}
+
+int main() 
+{
+    const int M = 14;
+    const int N = 6;
+    const int K = 64;
+
+    float A[M * K];
+    float B[K * N];
+    float C[M * N];
+
+    // Initialize matrices
+    for ( int i = 0; i < M * K; ++i )
+    {
+        A[i] = static_cast<float>( i );
+    }
+    for ( int j = 0; j < K * N; ++j )
+    {
+        B[j] = static_cast<float>( j );
+    }
+    std::memset( C, 0, sizeof( C ) );
+
+    int64_t l_iter = 10000 * 2000;
+    std::string v1_matmul_14( "v1_matmul_14" );
+    std::string v2_matmul_14( "v2_matmul_14" );
+    std::string v3_matmul_14( "v3_matmul_14" );
+    std::string v4_matmul_14( "v4_matmul_14" );
+    std::string g_matmul_14( "g_matmul_14" );
+
+    std::cout << "\nBenchmarking V1_Matmul_14_6_64 throughput ...\n";
+    benchmark_thr( l_iter, v1_matmul_14, A, B, C );
+    std::memset( C, 0, sizeof( C ) );
+
+    std::cout << "\nBenchmarking V2_Matmul_14_6_64 throughput ...\n";
+    benchmark_thr( l_iter, v2_matmul_14, A, B, C );
+    std::memset( C, 0, sizeof( C ) );
+
+    std::cout << "\nBenchmarking V3_Matmul_14_6_64 throughput ...\n";
+    benchmark_thr( l_iter, v3_matmul_14, A, B, C );
+    std::memset( C, 0, sizeof( C ) );
+
+    std::cout << "\nBenchmarking V4_Matmul_14_6_64 throughput ...\n";
+    benchmark_thr( l_iter, v4_matmul_14, A, B, C );
+    std::memset( C, 0, sizeof( C ) );
+
+    std::cout << "\nBenchmarking Matmul_M_6_64 M=14 throughput ...\n";
+    benchmark_thr( l_iter, g_matmul_14, A, B, C );
+    std::memset( C, 0, sizeof( C ) );
+
+
+    const int m = 15;
+    const int n = 6;
+    const int k = 64;
+
+    float a[m * k];
+    float b[k * n];
+    float c[m * n];
+
+    // Initialize matrices
+    for ( int i = 0; i < m * k; ++i )
+    {
+        a[i] = static_cast<float>( i );
+    }
+    for ( int j = 0; j < k * n; ++j )
+    {
+        b[j] = static_cast<float>( j );
+    }
+    std::memset( c, 0, sizeof( c ) );
+
+    std::string v1_matmul_15( "v1_matmul_15" );
+    std::string v2_matmul_15( "v2_matmul_15" );
+    std::string v3_matmul_15( "v3_matmul_15" );
+    std::string g_matmul_15( "g_matmul_15" );
+
+    std::cout << "\nBenchmarking V1_Matmul_15_6_64 throughput ...\n";
+    benchmark_thr( l_iter, v1_matmul_15, a, b, c );
+    std::memset( c, 0, sizeof( c ) );
+
+    std::cout << "\nBenchmarking V2_Matmul_15_6_64 throughput ...\n";
+    benchmark_thr( l_iter, v2_matmul_15, a, b, c );
+    std::memset( c, 0, sizeof( c ) );
+
+    std::cout << "\nBenchmarking V3_Matmul_15_6_64 throughput ...\n";
+    benchmark_thr( l_iter, v3_matmul_15, a, b, c );
+    std::memset( c, 0, sizeof( c ) );
+
+    std::cout << "\nBenchmarking Matmul_M_6_64 M=15 throughput ...\n";
+    benchmark_thr( l_iter, g_matmul_15, a, b, c );
+    std::memset( c, 0, sizeof( c ) );
+
+    return 0;
+}
